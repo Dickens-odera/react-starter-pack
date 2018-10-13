d9b45597cb39585fcf15e612a9934a13d999c34d
Dickens-odera <dickensodera9@gmail.com>
2018-10-14 01:51:08 +0300

    adding more stylsheets

--- src/ContactForm/Contact.jsx
+++ src/ContactForm/Contact.jsx
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
 import './contact.css';
 
+
 class Contact extends Component{
     render(){
         return (
-            <div className="contactForm">
+            <div className="contactForm-div">
+            <div className="form-wrapper">
                 <div className="form-group">
                     <input type="text" name="username" id="username-field" placeholder="Enter username"/>
+                    </div>
+                    <div className="form-group">
                     <input type="email" name="email" id="email-field" placeholder="example@gmail.com"/>
+                    </div>
+                    <div className="form-group">
                     <textarea name="message" id="message-field" placeholder=""></textarea>
-
+                    </div>
+                    <div className="form-group">
+                    <input type="submit" value="Save"/>
+                    </div>
                 </div>
-            </div>
+                </div>
+            
         );
     }
 }
