80af6dc45b207c84c785826cbb25419f8825ae52
Dickens-odera <dickensodera9@gmail.com>
2018-10-15 17:02:11 +0300

    Adding react-redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 //import Members from './ContactForm/members';
 import Header from './ContactForm/headers';
 import Contact from './ContactForm/Contact.jsx';
+
+//react-redux libraries
+
+import {Provider} from 'react-redux';
 //import Info from './ContactForm/info';
 //import Footer from './ContactForm/Footer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store = {store}>     
+<div className="App">
       <Header />
         <Contact />
       </div>
+      </Provider>
     );
   }
 }
