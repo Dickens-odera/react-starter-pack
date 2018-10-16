d86c9812a538c91c17554c0c2c6f9ae12887f740
Dickens-odera <dickensodera9@gmail.com>
2018-10-16 13:17:41 +0300

    Modifying the Contact form in the Contact component

--- src/ContactForm/Contact.jsx
+++ src/ContactForm/Contact.jsx
@@ -38,7 +38,7 @@ class Contact extends Component{
                     <div className="brand">
                         <h3 className="text-center responsive bg-bright header-text" id="msg-div">Contact Us</h3>
                     </div>
-                        <form className="form-group" action="" onSubmit={this.onSubmit}>
+                        <form action="" onSubmit={this.onSubmit}>
                             <div className="form-group">
                                 <input type="text" name="username" placeholder="Enter username" className="form-control" id="username"
                                 value={this.state.username}
