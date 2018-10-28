96c9bfba7da1cf105bbd5d3127e95f6a57443285
Dickens-odera <dickensodera9@gmail.com>
2018-10-28 05:14:03 +0300

    Connecting the express to mongodb database

--- server/server.js
+++ server/server.js
@@ -2,11 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 const bodyParser = require("body-parser");
-
+const conf = require("./database/DB");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//connect to the mongodb database
+mongoose.connect(conf.DB).then(
+    ()=>{
+        console.log("Database connected")
+    },
+    error =>{
+        console.log("Could not connect to the database "+error );
+    }
+);
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
