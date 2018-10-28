6d354dae783714a89a515d4c58d34a11d714093d
Dickens-odera <dickensodera9@gmail.com>
2018-10-28 04:50:28 +0300

    Working on the backend with express and mongodb

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require('cors');
 const bodyParser = require("body-parser");
+
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function(){
-    console.log("Server running on port "+ PORT);
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.get('/',(req,res)=>{
+    res.send("Hello");
 });
+
+app.listen(PORT, function(){
+    console.log("Server running on port "+PORT);
+});
+/*
+app.listen(PORT,()=>{
+    console.log(`Server running on port `&{PORT});
+});
+*/
