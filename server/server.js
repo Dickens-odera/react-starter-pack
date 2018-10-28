7f7f257df2c9b4e2a59f349a802c51cd5c78283d
Dickens-odera <dickensodera9@gmail.com>
2018-10-28 05:54:04 +0300

    using ES6/ES7 js for server functions

--- server/server.js
+++ server/server.js
@@ -3,13 +3,20 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const conf = require("./database/DB");
-
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+/*
+mongoose.connect(conf.DB).then(
+    ()=>{
+        console.log('Connected to mongodb databse');
+    }
+)
+*/
 
 //connect to the mongodb database
-mongoose.connect(conf.DB).then(
+MongoClient.connect(conf.DB).then(
     ()=>{
         console.log("Database connected")
     },
